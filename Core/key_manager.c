9e865c8d7dbe721b47320160994630fe0ef00791
Wuchuyuan <1917507415@qq.com>
2026-01-27 17:25:49 +0800

    修复睡眠时功耗过高的问题

--- Core/key_manager.c
+++ Core/key_manager.c
@@ -106,7 +106,7 @@ void user_key_Init(void)
     // 充电满按键输入
     GPIO_InitStruct.Pin = FULL_CHARGE_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(FULL_CHARGE_GPIO_PORT, &GPIO_InitStruct);
 
--- Core/pwr_stop.c
+++ Core/pwr_stop.c
@@ -31,7 +31,7 @@ static void pwr_stop_Init(void)
     // 充电满按键输入
     GPIO_InitStruct.Pin = FULL_CHARGE_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(FULL_CHARGE_GPIO_PORT, &GPIO_InitStruct);
 
